45a1e791d7cefcbb6ef6e7a16476930a423f4a29
Eric <eferrer23@gmail.com>
2016-01-22 23:21:28 -0800

    moar chang

---
+++ public/js/another.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function() {
+  $('.mobile-nav').click(function() {
+    $('.main-container').toggleClass('menu-active');
+    $('.mobile-nav').toggleClass('menu-active');
+    $('.side-nav').toggleClass('menu-active');
+  });
