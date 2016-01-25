436de35e57ad7ed586fe81ad7a890a3412df4b63
Eric <eferrer23@gmail.com>
2016-01-25 00:01:22 -0800

    k

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 80);
 
 
 app.get('/',function(req,res,next){
