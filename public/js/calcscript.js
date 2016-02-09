225eb46f2421e1fcb1c6c82d4bf8930f5980000d
Eric <eferrer23@gmail.com>
2016-02-09 01:52:06 -0800

    more shenangigans(sp?)

--- public/js/calcscript.js
+++ public/js/calcscript.js
@@ -1,59 +1,89 @@
 
-/*alert("!!!");
-$(document).ready(function(){
-
-
-  $(".clr").click(function() {
-      alert("fml");
-  });
-
-    });
-    
-    
-*/
-
 document.addEventListener("DOMContentLoaded", function (event) {
     
+    var operation1;
+    var convert;
+    var num1;
+    var num2;
+    
     $(".clearentry").click(function(){
-        $(".output").val("BOOM");
-        alert("derp");    
+        $(".output").val(""); 
     });
     
-    $(".zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine").click(function(){
-        var yoink = $(this).val();
-        $('.output').val($('.output').val() + yoink)
+    $(".clearall").click(function(){
+        $(".output").val(""); 
+        num1 = 0;
+        num2 = 0;
+    });
+    
+    
+    $(".zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine").click(function(){      
+        $('.output').val($('.output').val() + $(this).val());        
+    });
+    
+    $(".posneg").click(function(){  
+        convert = $(".output").val();
+        var newb = parseFloat(convert) * -1;
+        $('.output').val(newb);      
+        
+    });   
+    
+    $(".operation").click(function() {
+        num1 = $(".output").val();
+        $(".output").val("");
+        operation1 = $(this).attr(`name`);               
+    });
+        
+    $(".calculate").click(function(){
+        num2 = $(".output").val();
+        if(operation1 === 'add')
+            {   
+               result = add(num1, num2);
+               $(".output").val(result);
+            }
+        if(operation1 === 'subtract')
+            {   
+               result = subtract(num1, num2);
+               $(".output").val(result);
+            }
+        if(operation1 === 'multiply')
+            {   
+               result = multiply(num1, num2);
+               $(".output").val(result);
+            }
+            if(operation1 === 'divide')
+            {   
+               result = divide(num1, num2);
+               $(".output").val(result);
+            }
         
     });
     
-    $("add").click(function(){
-        var a = $(output).val();
-        
-        var b = $(output).val();
-        var result = add(a, b);
-         $(".output").val(result);
-        
-        
-        
-        
-    });
-    
-    $("subtract").click(function(){
-        
-    });
-    
-     $("multiply").click(function(){
-        
-    });
-    
-    $("divide").click(function(){
-        
-    });
+
     
 });
 
-function add(a,b){
-    return a + b;
+
+function add(a, b){
+   return parseInt(a) + parseInt(b);
+   // return a + b;
     
 }
+
+function subtract(a, b){
+   return parseInt(a) - parseInt(b);
+   // return a + b;
     
- 
+}
+
+ function multiply(a, b){
+   return parseInt(a) * parseInt(b);
+   // return a + b;
+    
+}
+
+function divide(a, b){
+   return parseInt(a) / parseInt(b);
+   // return a + b;
+    
+}
