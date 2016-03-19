589b3b7cfd2b7052558026ac90682aa6c8e53588
Eric <eferrer23@gmail.com>
2016-03-19 10:50:56 -0700

    k

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.get('/reset-table', function (req, res, next) {
         console.log("yikes");
         mysql.pool.query(createString, function (err) {
             context.results = "Table reset";
-            res.render('home', context);
+            res.render('exercise', context);
             //res.sendFile('public/htmlform.html', {root: __dirname
 
 
